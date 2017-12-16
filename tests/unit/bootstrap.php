fa1d42b5c5119a293ae7d7fe65b188dc18c4cec2
Christoph M. Becker <cmbecker69@gmx.de>
2017-12-16 12:16:14 +0100

    Cater to cmb69/pfw_xh@79905be7d8de5343c44406690bfd5fe284c3a665
    We remove the unit tests which are heavily relying on mocking functions
    for now.

--- tests/unit/bootstrap.php
+++ tests/unit/bootstrap.php
@@ -2,5 +2,3 @@
 
 require_once '../pfw/vendor/autoload.php';
 require_once '../pfw/classes/required_classes.php';
-require_once '../../cmsimple/adminfuncs.php';
-require_once '../../cmsimple/functions.php';
