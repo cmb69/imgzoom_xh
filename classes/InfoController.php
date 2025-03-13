899160cec2431c15b047dd80745a56dd5ff6ad7c
Christoph M. Becker <cmbecker69@gmx.de>
2025-03-13 19:25:27 +0100

    Use Plib_XH SystemChecker instead of Pfw_XH classes

--- classes/InfoController.php
+++ classes/InfoController.php
@@ -21,29 +21,86 @@
 
 namespace Imgzoom;
 
-use Pfw\SystemCheckService;
+use Plib\SystemChecker;
 use Plib\View;
 
 class InfoController
 {
+    /** @var SystemChecker */
+    private $systemChecker;
+
+    /** @var View */
+    private $view;
+
+    public function __construct()
+    {
+        global $pth, $plugin_tx;
+
+        $this->systemChecker = new SystemChecker();
+        $this->view = new View("{$pth["folder"]["plugins"]}imgzoom/views/", $plugin_tx["imgzoom"]);
+    }
+
     /**
      * @return void
      */
     public function defaultAction()
     {
-        global $pth, $plugin_tx;
+        global $pth;
 
-        $view = new View("{$pth["folder"]["plugins"]}imgzoom/views/", $plugin_tx["imgzoom"]);
-        echo $view->render('info', [
+        echo $this->view->render('info', [
             'logo' => "{$pth['folder']['plugins']}imgzoom/imgzoom.png",
             'version' => Plugin::VERSION,
-            'checks' => (new SystemCheckService)
-                ->minPhpVersion('5.4.0')
-                ->minXhVersion('1.6.3')
-                ->plugin('pfw')
-                ->writable("{$pth['folder']['plugins']}imgzoom/css/")
-                ->writable("{$pth['folder']['plugins']}imgzoom/languages/")
-                ->getChecks()
+            'checks' => [
+                $this->checkPhpVersion('5.4.0'),
+                $this->checkXhVersion('1.6.3'),
+                $this->checkPlib(),
+                $this->checkWritability("{$pth['folder']['plugins']}imgzoom/css/"),
+                $this->checkWritability("{$pth['folder']['plugins']}imgzoom/languages/"),
+            ],
         ]);
+    }
+
+    /** @return array{class:string,label:string,stateLabel:string} */
+    private function checkPhpVersion(string $version): array
+    {
+        $state = $this->systemChecker->checkVersion(PHP_VERSION, $version) ? 'success' : 'fail';
+        return [
+            'class' => "xh_$state",
+            'label' => $this->view->plain('syscheck_phpversion', $version),
+            'stateLabel' => $this->view->plain("syscheck_$state"),
+        ];
+    }
+
+    /** @return array{class:string,label:string,stateLabel:string} */
+    private function checkXhVersion(string $version): array
+    {
+        $state = $this->systemChecker->checkVersion(CMSIMPLE_XH_VERSION, "CMSimple_XH $version") ? 'success' : 'fail';
+        return [
+            'class' => "xh_$state",
+            'label' => $this->view->plain('syscheck_xhversion', $version),
+            'stateLabel' => $this->view->plain("syscheck_$state"),
+        ];
+    }
+
+    /** @return array{class:string,label:string,stateLabel:string} */
+    private function checkPlib()
+    {
+        $state = $this->systemChecker->checkPlugin("plib") ? 'success' : 'fail';
+        return [
+            'class' => "xh_$state",
+            'label' => $this->view->plain('syscheck_plugin', 'Plib_XH'),
+            'stateLabel' => $this->view->plain("syscheck_$state"),
+        ];
+    }
+
+    /** @return array{class:string,label:string,stateLabel:string} */
+    private function checkWritability(string $folder): array
+    {
+        $state = $this->systemChecker->checkWritability($folder) ? 'success' : 'warning';
+        return [
+            'class' => "xh_$state",
+            'label' => $this->view->plain('syscheck_writable', $folder),
+            'stateLabel' => $this->view->plain("syscheck_$state"),
+        ];
     }
 }
--- views/info.php
+++ views/info.php
@@ -26,6 +26,6 @@
 <div class="pfw_syscheck">
     <h2><?=$this->text('syscheck_title')?></h2>
 <?php foreach ($checks as $check):?>
-    <p class="xh_<?=$check->getState()?>"><?=$this->text('syscheck_message', $check->getLabel(), $check->getStateLabel())?></p>
+    <p class="<?=$check['class']?>"><?=$this->text('syscheck_message', $check['label'], $check['stateLabel'])?></p>
 <?php endforeach?>
 </div>
