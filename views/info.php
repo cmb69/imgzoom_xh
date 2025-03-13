d4439c43e05adee6d91e3f555f61c26c67c93af6
Christoph M. Becker <cmbecker69@gmx.de>
2025-03-13 19:26:14 +0100

    Drop useless (and annoying) license info from info screen

--- views/info.php
+++ views/info.php
@@ -1,29 +1,5 @@
-<h1>Imgzoom</h1>
-<img class="pfw_logo" src="<?=$logo?>" alt="<?=$this->text('alt_icon')?>">
-<p>
-    Version: <?=$version?>
-</p>
-<p>
-    Copyright © 2014-2017 Christoph M. Becker
-</p>
-<p class="pfw_license">
-    This program is free software: you can redistribute it and/or modify it
-    under the terms of the GNU General Public License as published by the Free
-    Software Foundation, either version 3 of the License, or (at your option)
-    any later version.
-</p>
-<p class="pfw_license">
-    This program is distributed in the hope that it will be useful, but
-    <em>without any warranty</em>; without even the implied warranty of
-    <em>merchantibility</em> or <em>fitness for a particular purpose</em>. See
-    the GNU General Public License for more details.
-</p>
-<p class="pfw_license">
-    You should have received a copy of the GNU General Public License along with
-    this program. If not, see <a href="http://www.gnu.org/licenses/"
-    target="_blank">http://www.gnu.org/licenses/</a>.
-</p>
-<div class="pfw_syscheck">
+<h1>Imgzoom <?=$this->esc($version)?></h1>
+<div>
     <h2><?=$this->text('syscheck_title')?></h2>
 <?php foreach ($checks as $check):?>
     <p class="<?=$check['class']?>"><?=$this->text('syscheck_message', $check['label'], $check['stateLabel'])?></p>
